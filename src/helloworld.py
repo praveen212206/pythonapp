19826a2f532130d88ffc858f08f85771e3e6b67c
praveen212206 <167956964+praveen212206@users.noreply.github.com>
2024-07-18 18:20:51 +0530

    Update helloworld.py

--- src/helloworld.py
+++ src/helloworld.py
@@ -6,7 +6,7 @@ api = Api(app)
 
 class Greeting (Resource):
     def get(self):
-        return 'Welcome to World of Peace'!
+        return 'Welcome to World of Peace'
 
 api.add_resource(Greeting, '/') # Route_1
 
